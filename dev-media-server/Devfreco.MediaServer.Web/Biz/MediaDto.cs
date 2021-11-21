719f8dd7b4783bb64fd8cb81329c738362a16c83
Mehmet <unal.m1991@gmail.com>
2021-11-21 17:30:34 +0300

    bugfix

---
+++ dev-media-server/Devfreco.MediaServer.Web/Biz/MediaDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Devfreco.MediaServer.Web.Biz
+{
+    public class MediaDto
+    {
+        public long FileSize { get; set; }
+        public string FileType { get; set; }
+        public string FileExt { get; set; }
+    }
+}
---
+++ dev-media-server/Devfreco.MediaServer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Devfreco.MediaServer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
