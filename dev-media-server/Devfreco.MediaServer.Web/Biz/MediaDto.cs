f82913444cc5210ff132cc5dd3622b3abee023fa
Mehmet <unal.m1991@gmail.com>
2021-11-23 00:30:03 +0300

    okişşşş

--- dev-media-server/Devfreco.MediaServer.Web/Biz/MediaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Devfreco.MediaServer.Web.Biz
-{
-    public class MediaDto
-    {
-        public long FileSize { get; set; }
-        public string FileType { get; set; }
-        public string FileExt { get; set; }
-    }
-}
